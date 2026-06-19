0c594e3f236995eeea9731ed125bf79111c005d6
Hostbug <112290681+Hostbug@users.noreply.github.com>
2026-06-20 07:55:16 +0800

    Enable sakura effect in sakuraConfig

--- src/config/sakuraConfig.ts
+++ src/config/sakuraConfig.ts
@@ -2,7 +2,7 @@ import type { SakuraConfig } from "../types/config";
 
 export const sakuraConfig: SakuraConfig = {
 	// 是否启用樱花特效
-	enable: false,
+	enable: true,
 	// 是否允许用户通过控制面板切换樱花动画
 	switchable: true,
 
