5d9d761b8ec147ee93f465dfe52d936fe8925dea
Hostbug <3108455619@qq.com>
2026-07-27 21:34:27 +0800

    更新内容

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -78,7 +78,7 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 			},
 			{
 				name: "QQ交流群",
-				url: "#",
+				url: "https://qun.qq.com/universal-share/share?ac=1&authKey=N%2BUmyI2%2BkO%2Fejdbuz22vu1xjgFgjKGCG93D9NeRYopY1y3mZktEeyheDkHaC%2Ffb9&busi_data=eyJncm91cENvZGUiOiIxMDkwNjM0ODU0IiwidG9rZW4iOiJjcVF3VWVxN25uQTZSeG9Hdkw2cHQ4enZnc1BHQzlvZHRFcVFUa3Nzbi9XZ3pWYnFab2hhSno0a1E3aCs4eHBjIiwidWluIjoiMzEwODQ1NTYxOSJ9&data=9FvUF0VcbpjWEuI2UciqDqv3VFj_n3hou9QobTktV19AxNBmv8R1CHybCVy6opXjGQxgx8xp3uPUbjO4BA71jw&svctype=4&tempid=h5_group_info",
 				external: true,
 				icon: "fa7-brands:qq",
 			},
