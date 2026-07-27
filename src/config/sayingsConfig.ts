a6c85a6d9982233a083062bbc3df69354b7fba13
Hostbug <112290681+Hostbug@users.noreply.github.com>
2026-07-28 01:40:41 +0800

    admin: delete saying #5

--- src/config/sayingsConfig.ts
+++ src/config/sayingsConfig.ts
@@ -28,20 +28,9 @@ export const sayingsData: Saying[] = [
     id: 4,
     content: "我经常会在睡不着的时候反思，反思我走过的路，我做过的决定，如果可以重来的话，我会不会做出不同的选择。我也会在某一个瞬间觉得自己走错了很多路。",
     media: [
-      { type: "video", url: "/assets/videos/1074.mp4" }
+    { type: "video", url: "/assets/videos/1074.mp4" }
   ],
     time: "2026-07-28 00:00",
-    location: "127.0.0.1",
-    likes: 520,
-    tags: ["日常"],
-  },
-  {
-    id: 5,
-    content: "test img 1",
-    media: [
-    { type: "image", url: "http://gallery.hwh.ccwu.cc/img/a8a3551bf4fb.jpg" }
-  ],
-    time: "2026-07-28 00:11",
     location: "127.0.0.1",
     likes: 520,
     tags: ["日常"],
