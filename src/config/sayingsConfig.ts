c555064b90bfbb610fc9961875914bb6b254ffc6
Hostbug <112290681+Hostbug@users.noreply.github.com>
2026-07-28 00:08:30 +0800

    admin: create saying #4

--- src/config/sayingsConfig.ts
+++ src/config/sayingsConfig.ts
@@ -23,5 +23,16 @@ export const sayingsData: Saying[] = [
     location: "127.0.0.1",
     likes: 520,
     tags: ["日常"],
+  },
+  {
+    id: 4,
+    content: "我经常会在睡不着的时候反思，反思我走过的路，我做过的决定，如果可以重来的话，我会不会做出不同的选择。我也会在某一个瞬间觉得自己走错了很多路。",
+    media: [
+    { type: "video", url: "https://photovideo.photo.qq.com/1074_0b53u5dxhoiar4ad4lthmvvdfjyeonsaan2a.f0.mp4?dis_k=2d47bfc4eb60d8ce1ea4e4b5608966b4&dis_t=1785168404&vuin=3108455619&owner=MzEwODQ1NTYxOQ==" }
+  ],
+    time: "2026-07-28 00:00",
+    location: "127.0.0.1",
+    likes: 520,
+    tags: ["日常"],
   }
 ];
